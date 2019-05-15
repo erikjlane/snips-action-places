9eef09ba0b816855e475fc6842abdb715b5a9185
Thomas Bouvier <thomas.bouvier@snips.ai>
2019-05-15 17:04:40 +0200

    Disabled debug printing

--- action-places.js
+++ action-places.js
@@ -2,7 +2,9 @@
 const debug = require('debug')
 const { name } = require('./package.json')
 
+// Enable error print
+debug.enable(name + ':error')
 // Uncomment this line to print everything
-debug.enable(name + ':*')
+// debug.enable(name + ':*')
 
 require('./src/index')()
